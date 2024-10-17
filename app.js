3a95dcf05224f458a1f9c3ffc0d853f70f44fb26
ShajadurR <shajadurrrahman@gmail.com>
2024-10-17 19:12:08 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Practice Problem 3
+
+// You task is to divide the given number by 5 and show the remainder as the outpu
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+// Output:
+// 4
+
+let givenNumber = 119
+const remainder = givenNumber % 5;
+console.log(remainder);
